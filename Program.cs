28d877cbb8a631dc66e3f18952094a7a54fc0faf
MDTVisit <112145096+MDTVisit@users.noreply.github.com>
2022-12-12 23:36:50 +0700

    58964565adad

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Tree<string> tree = new Tree<string>();
+
+
+        while(true){
+            string name = Console.ReadLine();
+            int node = int.Parse(Console.ReadLine());
+
+            
+        Console.Write(name);
+
+
+       }
+    }
+}
