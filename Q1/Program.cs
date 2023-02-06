57e85c7ed27d03c3fa4d4c24cdf14c5c82ca5d5b
MDTVisit <112145096+MDTVisit@users.noreply.github.com>
2023-02-06 22:10:35 +0700

    adadadad

---
+++ Q1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        double n = double.Parse(Console.ReadLine());
+
+        Console.WriteLine (F(n));
+
+
+    }
+
+
+    static double F(double X)
+    {
+        if (X*X <= 4)
+        {return 2;}
+
+
+        return 1 + F(X - ((4 * Math.Abs(X)) / X));
+    }
+
+} 
